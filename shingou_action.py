4cca2e240fc07f520de8ad0c2eb596e709a2707b
hoge <2104240033yt@edu.oca.ac.jp>
2023-12-02 13:53:51 +0900

    列挙型演習

---
+++ shingou_action.py
@@ -0,0 +1,16 @@
+from enum import Enum
+class Shingou(Enum):
+    RED = 1
+    BLUE = 2
+    YELLOW = 3
+
+def act_shingou(color):
+    if color == 1:
+        print("とまれ")
+    elif color == 2:
+        print("すすめ")
+    elif color == 3:
+        print("ちゅうい") 
+    else:
+        raise Exception("信号機の色に対応していません")
+    return Shingou(color)
---
+++ test_command.py
@@ -0,0 +1,6 @@
+import sys
+def test_command():
+	args = sys.argv
+	print(args[0])#←「test_command.py」が出力
+	print(args[1])#←「a」が出力
+	print(args[2])#←「b」が出力
